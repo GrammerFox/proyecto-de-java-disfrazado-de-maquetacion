83c77ccac568575163fa047609cf00f5808f682c
Ernesto56734856 <provisionalernesto@gmail.com>
2025-07-29 19:12:38 -0500

    lo que no pude terminar el marte

---
+++ pop/sufrimiento.js
@@ -0,0 +1,19 @@
+let elementos = ['pedro', 'pablo', 'alberto', 'mariano', 'hermenegildo', 'carmelo', 'bombon', 'burbuja', 'bellota']
+
+const popbutton = document.getElementById (popbutton)
+const resetbutton = document.getElementById (resetbutton)
+
+function funcionDePop () {
+
+
+
+}
+
+function funcionDeReseteo () {
+
+
+
+}
+
+resetbutton.addEventListener ('click', funcionDePop)
+resetbutton.addEventListener ('click',funcionDeReseteo) 
